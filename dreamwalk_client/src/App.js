f60c0e74daa21a3667c1cab5fcefdabba0ba087d
Blair <blairacarroll@gmail.com>
2020-03-09 02:20:03 -0400

    new img and components dir setup with css changes to login

--- dreamwalk_client/src/App.js
+++ dreamwalk_client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="main-container">
-      
+    <div className="login-overlay">
+      <Login />
     </div>
   );
 }
---
+++ dreamwalk_client/src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+    render(){
+        return(
+            <div>
+                <div>
+                    <h3 style={{backgroundColor: 'white', width: '200px', marginLeft: '10px'}}><a href="#">&#9855; Better accessibility</a></h3>
+                </div>
+                <div className="login-container">
+                    <form onSubmit={this.handleSubmit}>
+                        <label></label>
+                        <input placeholder="Login" id="input-login"></input>
+                        <br />
+                        <label></label>
+                        <input placeholder="password" id="input-login"></input>
+                        <br />
+                        <button id="submit-login-button">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
